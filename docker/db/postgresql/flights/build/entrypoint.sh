23c664af4a1aba295443425625df8dc2168aed6c
marek.sunda <marek.sunda@gmail.com>
2022-11-30 20:09:22 +0100

    LF line endings for docker - shell script needs them, careful :)

--- docker/db/postgresql/flights/build/entrypoint.sh
+++ docker/db/postgresql/flights/build/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# run default entrypoint script
+# run default entrypoint script so the DB spins up
 ./usr/local/bin/docker-entrypoint.sh
 # Load data into PostreSQL
 psql -f /tmp/data-db/demo-small-en-20170815.sql -U postgres
